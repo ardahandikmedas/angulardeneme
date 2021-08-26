bd22cb76fe5681137a40fdaec7aa30a267779472
Ardahan Dikmedas <arda75.ad@gmail.com>
2021-08-26 18:02:17 +0300

    commit3

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -8,43 +8,72 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CustomersComponent implements OnInit {
 
-  userForm :FormGroup
-  id: number;
-  name: string;
-  surname: string;
-  email: string;
-  age: number;
-  tableData: any;
-  
-
-  constructor(private fb:FormBuilder) { 
-
-    this.tableData = [];
-
-    this.userForm = this.fb.group ({
-      id: [],
-      name :['', Validators.required],
-      surname: ['', Validators.required],
-      email: ['', Validators.required],
-      age: ['', Validators.required]
-    });
-  }
-
-  ngOnInit(): void {
-   
-  }
-  addData() {
-    this.tableData.push(this.userForm.value);
-  }
-  editData() {
-    
-
-  }
-  deleteData(email:string) {
-    for (let i = 0; i < this.tableData.length; ++i) {
-      if (this.tableData[i].email === email) {
-        this.tableData.splice(i, 1);
-      }
-    }
-  }
+ userForm: FormGroup;
+ editForm: FormGroup;
+ id: number;
+ name: string;
+ surname: string;
+ email: string;
+ age: number;
+ tableData: any;
+ 
+ 
+ lastId: number = 0;
+ 
+ editingCustomerId: number;
+ 
+ constructor(private fb: FormBuilder) {
+ this.tableData = [];
+ 
+ this.userForm = this.fb.group({
+ id: [],
+ name: ['', Validators.required],
+ surname: ['', Validators.required],
+ email: ['', Validators.required],
+ age: ['', Validators.required]
+ });
+ 
+ this.editForm = this.fb.group({
+ id: [],
+ name: ['', Validators.required],
+ surname: ['', Validators.required],
+ email: ['', Validators.required],
+ age: ['', Validators.required]
+ });
+ }
+ 
+ ngOnInit(): void {}
+ addData() {
+ let customer = this.userForm.value;
+ customer.id = ++this.lastId;
+ this.tableData.push(customer);
+ }
+ editData(id: number) {
+ let customer = this.tableData.find((data:any) => data.id == id);
+ this.editForm.setValue({
+ id: customer.id,
+ name: customer.name,
+ surname: customer.surname,
+ email: customer.email,
+ age: customer.age
+ });
+ this.editingCustomerId = customer.id;
+ }
+ 
+ updateData() {
+ let customerIndex = this.tableData.findIndex((data:any) => data.id == this.editingCustomerId);
+ let updatedCustomer = this.editForm.value;
+ 
+ this.tableData.splice(customerIndex, 1);
+ this.tableData.push(updatedCustomer);
+ 
+ console.log(this.tableData);
+ }
+ deleteData(id: number) {
+ for (let i = 0; i < this.tableData.length; ++i) {
+ if (this.tableData[i].id === id) {
+ this.tableData.splice(i, 1);
+ }
+ }
+ }
 }
